3f3487eed9a8c63bd1a1a4a1645dff83b6d02bad
Paul Vudmaska <Paul.Vudmaska@Rackspace.com>
2013-02-02 15:40:01 -0600

    init repos

---
+++ controller.rb
@@ -0,0 +1,10 @@
+ignore /\/views/
+
+helpers do
+  def request_quote
+  	haml_tag "a", :rel => 'tooltip', :title => 'Request a quote' do
+  		haml_tag "i", :class => 'icon-question-sign'
+  	end
+  	#{}%{<a href='#' rel='tooltip' title='Request a quote'><i class='icon-question-sign'/></a>}           
+  end
+end
